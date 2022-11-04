d2de6e5ba76fd008fe6db8e2783cbbedf6a598a0
surferdargent <olecurieux@gmail.com>
2022-11-04 06:40:06 +0100

    V3

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ TEAM_MEMBERS = [
     Member(
         name="Jérémy Bitton, Frédéric Constant, Olivier Le Curieux Clerville",
         linkedin_url="https://www.linkedin.com/",
-        github_url="https://github.com/surferdargent/Projet_Datascientest/",
+        github_url="https://github.com/surferdargent/Projet_Soutenance_Ecole_Datascientest_Octobre_2022/edit/main/README.md",
     )]
 
 PROMOTION = "Promotion Continue     Data Analyste - Mars 2022"
