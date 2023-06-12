d8536b5dbd6070066512b9387e732a5722568b60
surferdargent <olecurieux@gmail.com>
2023-06-12 10:10:54 +0200

    V7

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
+"""
+Created on Sun Oct 16 16:16:13 2022.
+
+@author: olecu
+"""
+
+# -*- coding: utf-8 -*-
+from collections import OrderedDict
 import streamlit as st
 import config
+from tabs import intro, second_tab, third_tab, fourth_tab
 st.set_page_config(
-    page_title=config.TITLE,
-    #page_icon="https://datascientest.com/wp-content/uploads/2020/03/cropped-favicon-datascientest-1-32x32.png",
+    page_title="Paris Sportif Tennis",
+    # page_icon="https://datascientest.com/wp-content/uploads/2020/03/cropped-favicon-datascientest-1-32x32.png",
 )
-
-
-from collections import OrderedDict
-
-from tabs import intro, second_tab, third_tab,fourth_tab
-
-
-
 
 with open("style.css", "r") as f:
     style = f.read()
@@ -19,7 +20,7 @@ with open("style.css", "r") as f:
 st.markdown(f"<style>{style}</style>", unsafe_allow_html=True)
 
 
-# TODO: add new and/or renamed tab in this ordered dict by
+# add new and/or renamed tab in this ordered dict by
 # passing the name in the sidebar as key and the imported tab
 # as value as follow :
 TABS = OrderedDict(
@@ -28,19 +29,18 @@ TABS = OrderedDict(
         (second_tab.sidebar_name, second_tab),
         (third_tab.sidebar_name, third_tab),
         (fourth_tab.sidebar_name, fourth_tab)
-        
     ]
 )
 
 
 def run():
+    """ Début du promgramme"""
     st.sidebar.image(
-        
         "https://dst-studio-template.s3.eu-west-3.amazonaws.com/logo-datascientest.png",
-        width=200
+        width=200,
     )
-    
-    #st.write("Battre les Bookmakers Tennis ?")
+
+    # st.write("Battre les Bookmakers Tennis ?")
     tab_name = st.sidebar.radio("", list(TABS.keys()), 0)
     st.sidebar.markdown("---")
     st.sidebar.markdown(f"## {config.PROMOTION}")
@@ -52,7 +52,9 @@ def run():
     tab = TABS[tab_name]
 
     tab.run()
-    st.sidebar.markdown("Sous le tutorat de *Gaspard Grimm*")
+    st.sidebar.markdown("**Sous le tutorat de Gaspard Grimm**")
+
 
 if __name__ == "__main__":
+
     run()
